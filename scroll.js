365deba9527081ae7adf9cfc56a1c48e5b118242
Sim3dn <61627289+Sim3dn@users.noreply.github.com>
2025-07-26 12:10:37 +0100

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray("section").forEach((section) => {
+    gsap.from(section, {
+      scrollTrigger: {
+        trigger: section,
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      y: 50,
+      duration: 0.8,
+      ease: "power2.out"
+    });
+  });
+});
